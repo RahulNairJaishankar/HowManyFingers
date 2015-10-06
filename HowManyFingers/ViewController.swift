0ae5d6106c8eae3a4fba8e948833756f553de2a4
Rahul Nair Jaishankar <rjaisha@purdue.edu>
2015-10-06 16:05:48 -0400

    First commit

--- HowManyFingers/ViewController.swift
+++ HowManyFingers/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var Guess: UITextField!
+    @IBOutlet var output: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func button(sender: AnyObject) {
+        
+        let userGuess:Int = Int(Guess.text!)!
+        
+        let noOfFingers:Int = Int(arc4random_uniform(6))
+        print(noOfFingers)
+        
+        if( userGuess == noOfFingers )
+        {
+            output.text = "You've guessed right! Guess again!"
+        }
+        else
+        {
+            output.text = "Wrong! It was \(noOfFingers). Guess again!"
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
